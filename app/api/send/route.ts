5053d7bdb792110f9eb51bb63167f35901dae287
limooo196 <limousine196@gmail.com>
2024-11-17 11:08:30 +0800

    pass resend API Key to contructor

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -4,8 +4,12 @@ import { Resend } from 'resend';
 
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+resend.apiKeys.create({ name: 'Production' });
+
 
 export async function POST() {
+
+  new Resend("prod")
   try {
     const { data, error } = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
